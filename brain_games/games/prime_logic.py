e1a5c85cc6f4a8e075794e8ad1c0cc7bd3ce74d3
Levon Kharajyan <l.kharajyan@gmail.com>
2023-06-25 11:42:04 +0300

    fix prime_logic.py

--- brain_games/games/prime_logic.py
+++ brain_games/games/prime_logic.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-TASK = 'Answer "yes" if given number is prime. Otherwise answer "no"'
+TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def game_logic():
